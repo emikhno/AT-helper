4a3c92f15f8c8c4a4abdd16f9df37fb99b1ad89b
Egor Mikhno <developer@emikhno.com>
2024-03-26 15:51:58 +0300

    Wrap js code in IIFE

--- js/work-page.js
+++ js/work-page.js
@@ -1,86 +1,89 @@
-const bookId = location.pathname.split('/')[2];
-let bookTypos = localStorage.getItem(`${extPrefix}typos_${bookId}`);
-const typosList = document.getElementById(`${extPrefix}typosList`);
+(function (AThelper) {
+    const extPrefix = AThelper.prefix;
+    const bookId = location.pathname.split('/')[2];
+    let bookTypos = localStorage.getItem(`${extPrefix}typos_${bookId}`);
+    const typosList = document.getElementById(`${extPrefix}typosList`);
 
-if (bookTypos) {
-    bookTypos = JSON.parse(bookTypos);
-    typosList.classList.add(`${extPrefix}d-block`);
-    typosList.addEventListener('click', () => {
-        openListModal();
-    });
-}
+    if (bookTypos) {
+        bookTypos = JSON.parse(bookTypos);
+        typosList.classList.add(`${extPrefix}d-block`);
+        typosList.addEventListener('click', () => {
+            openListModal();
+        });
+    }
 
 
 
-function openListModal() {
-    const typoListTitle = typoListModal.querySelector(`.${extPrefix}modal_title`);
-    typoListTitle.textContent = browser.i18n.getMessage("typosText");
-    const typoListBody = typoListModal.querySelector(`.${extPrefix}modal_body`);
-    typoListBody.innerHTML = '';
-    const typoListCopy = typoListModal.querySelector(`.${extPrefix}modal_actionMain`);
-    typoListCopy.textContent = browser.i18n.getMessage("copyText");
-    const typoListClear = typoListModal.querySelector(`.${extPrefix}modal_actionSecond`);
-    typoListClear.textContent = browser.i18n.getMessage("clearText");
+    function openListModal() {
+        const typoListTitle = AThelper.modal.querySelector(`.${extPrefix}modal_title`);
+        typoListTitle.textContent = browser.i18n.getMessage("typosText");
+        const typoListBody = AThelper.modal.querySelector(`.${extPrefix}modal_body`);
+        typoListBody.innerHTML = '';
+        const typoListCopy = AThelper.modal.querySelector(`.${extPrefix}modal_actionMain`);
+        typoListCopy.textContent = browser.i18n.getMessage("copyText");
+        const typoListClear = AThelper.modal.querySelector(`.${extPrefix}modal_actionSecond`);
+        typoListClear.textContent = browser.i18n.getMessage("clearText");
 
-    let prevChapter = '';
-    // let typosText = '';
+        let prevChapter = '';
+        // let typosText = '';
 
-    bookTypos.forEach((typo) => {
-        const typoElement = document.createElement('div');
-        const chapterName = document.createElement('p');
-        if (typo.chapterName !== prevChapter) {
-            chapterName.classList.add(`${extPrefix}bold`);
-            chapterName.classList.add(`${extPrefix}mt-2`);
-            chapterName.textContent = typo.chapterName;
-            // typosText += `${typo.chapterName}\n\n`;
-        } else {
-            chapterName.textContent = '...';
-            // typosText += '...\n\n';
-        }
-        typoElement.appendChild(chapterName);
-
-        const typoBody = document.createElement('p');
-        typoBody.innerHTML = `${typo.start}<b>[***]${typo.selected}[***]</b>${typo.end}`;
-        // typosText += `${typo.start}[***]${typo.selected}[***]${typo.end}\n\n`;
-        if (typo.typoDescription) {
-            typoBody.innerHTML += `<br><i>/${typo.typoDescription}/</i>`;
-            // typosText += `/${typo.typoDescription}/\n\n`;
-        }
-        typoElement.appendChild(typoBody);
-
-        typoListBody.appendChild(typoElement);
-        prevChapter = typo.chapterName;
-    });
-
-    typoListCopy.addEventListener('click', () => {
-        const commentField = document.querySelector('.fr-element');
-        if (commentField) {
-            commentField.focus();
-            typoListBody.childNodes.forEach(node => {
-                commentField.appendChild(node.cloneNode(true));
-            });
-
-            try {
-                const form = commentField.closest('form');
-                setTimeout(() => {
-                    const submitButton = form.querySelector('[data-bind="btn: processing, disable: disableSubmit"]');
-                    submitButton.removeAttribute('disabled');
-                });
-            } catch (error) {
-                console.log(form, submitButton);
+        bookTypos.forEach((typo) => {
+            const typoElement = document.createElement('div');
+            const chapterName = document.createElement('p');
+            if (typo.chapterName !== prevChapter) {
+                chapterName.classList.add(`${extPrefix}bold`);
+                chapterName.classList.add(`${extPrefix}mt-2`);
+                chapterName.textContent = typo.chapterName;
+                // typosText += `${typo.chapterName}\n\n`;
+            } else {
+                chapterName.textContent = '...';
+                // typosText += '...\n\n';
             }
-        }
-        typoListModal.classList.remove('AThelper__d-block');
-    });
+            typoElement.appendChild(chapterName);
 
-    typoListClear.addEventListener('click', () => {
-       const result = confirm(browser.i18n.getMessage("clearConfirm"));
-       if (result) {
-           localStorage.removeItem(`${extPrefix}typos_${bookId}`);
-           typosList.classList.remove(`${extPrefix}d-block`);
-           typoListModal.classList.remove('AThelper__d-block');
-       }
-    });
+            const typoBody = document.createElement('p');
+            typoBody.innerHTML = `${typo.start}<b>[***]${typo.selected}[***]</b>${typo.end}`;
+            // typosText += `${typo.start}[***]${typo.selected}[***]${typo.end}\n\n`;
+            if (typo.typoDescription) {
+                typoBody.innerHTML += `<br><i>/${typo.typoDescription}/</i>`;
+                // typosText += `/${typo.typoDescription}/\n\n`;
+            }
+            typoElement.appendChild(typoBody);
 
-    typoListModal.classList.add('AThelper__d-block');
-}
+            typoListBody.appendChild(typoElement);
+            prevChapter = typo.chapterName;
+        });
+
+        typoListCopy.addEventListener('click', () => {
+            const commentField = document.querySelector('.fr-element');
+            if (commentField) {
+                commentField.focus();
+                typoListBody.childNodes.forEach(node => {
+                    commentField.appendChild(node.cloneNode(true));
+                });
+
+                try {
+                    const form = commentField.closest('form');
+                    setTimeout(() => {
+                        const submitButton = form.querySelector('[data-bind="btn: processing, disable: disableSubmit"]');
+                        submitButton.removeAttribute('disabled');
+                    });
+                } catch (error) {
+                    console.log(form, submitButton);
+                }
+            }
+            AThelper.modal.classList.remove('AThelper__d-block');
+        });
+
+        typoListClear.addEventListener('click', () => {
+            const result = confirm(browser.i18n.getMessage("clearConfirm"));
+            if (result) {
+                localStorage.removeItem(`${extPrefix}typos_${bookId}`);
+                typosList.classList.remove(`${extPrefix}d-block`);
+                AThelper.modal.classList.remove('AThelper__d-block');
+            }
+        });
+
+        AThelper.modal.classList.add('AThelper__d-block');
+    }
+}(window.AThelper));
