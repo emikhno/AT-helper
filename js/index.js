835c7101e5e4cfa856c05ba1989b98c3c0f1591e
Egor Mikhno <developer@emikhno.com>
2024-03-30 21:11:58 +0300

    Create modal without innerHTML

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
     const mainMenu = document.createElement('div');
     mainMenu.classList.add(`${extPrefix}menu`);
     mainMenu.innerHTML =
-        `<svg id="${extPrefix}typoIcon" class="${extPrefix}cursor ${extPrefix}typoIcon" width="24px" height="24px"
+    `<svg id="${extPrefix}typoIcon" class="${extPrefix}cursor ${extPrefix}typoIcon" width="24px" height="24px"
         viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <title>${browser.i18n.getMessage("typoIconTitle")}</title>
         <path id="${extPrefix}typoIcon_path" d="M11 4H7.2C6.0799 4 5.51984 4 5.09202 4.21799C4.71569 4.40974 4.40973 4.7157 4.21799 5.09202C4 5.51985 4 6.0799 4 7.2V16.8C4 17.9201 4 18.4802 4.21799 18.908C4.40973 19.2843 4.71569 19.5903 5.09202 19.782C5.51984 20 6.0799 20 7.2 20H16.8C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V12.5M15.5 5.5L18.3284 8.32843M10.7627 10.2373L17.411 3.58902C18.192 2.80797 19.4584 2.80797 20.2394 3.58902C21.0205 4.37007 21.0205 5.6364 20.2394 6.41745L13.3774 13.2794C12.6158 14.0411 12.235 14.4219 11.8012 14.7247C11.4162 14.9936 11.0009 15.2162 10.564 15.3882C10.0717 15.582 9.54378 15.6885 8.48793 15.9016L8 16L8.04745 15.6678C8.21536 14.4925 8.29932 13.9048 8.49029 13.3561C8.65975 12.8692 8.89125 12.4063 9.17906 11.9786C9.50341 11.4966 9.92319 11.0768 10.7627 10.2373Z"
@@ -54,20 +54,49 @@
     // Create modal
     const extModal = AThelper.modal = document.createElement('div');
     extModal.classList.add(`${extPrefix}modal`);
-    extModal.innerHTML =
-        `<div class="${extPrefix}modal_content">
-        <div class="${extPrefix}modal_header">
-          <span class="${extPrefix}modal_close">&times;</span>
-          <h2 class="${extPrefix}modal_title"></h2>
-        </div>
-        <div class="${extPrefix}modal_body">
-        </div>
-        <div class="${extPrefix}modal_footer ${extPrefix}d-flex">
-            <button type="button" class="${extPrefix}modal_actionMain ${extPrefix}ml-auto ${extPrefix}mr-2 btn btn-primary"></button>
-            <button type="button" class="${extPrefix}modal_actionSecond ${extPrefix}mr-2 btn btn-danger"></button>
-            <button type="button" class="${extPrefix}modal_close btn btn-gray">${browser.i18n.getMessage("closeText")}</button>
-        </div>
-    </div>`
+    const modalContent = document.createElement('div');
+    modalContent.classList.add(`${extPrefix}modal_content`);
+
+    const modalHeader = document.createElement('div');
+    modalHeader.classList.add(`${extPrefix}modal_header`);
+    const modalCloseIcon = document.createElement('span');
+    modalCloseIcon.classList.add(`${extPrefix}modal_close`);
+    modalCloseIcon.textContent = 'X';
+    modalHeader.appendChild(modalCloseIcon);
+    const modalTitle = document.createElement('h2');
+    modalTitle.classList.add(`${extPrefix}modal_title`);
+    modalHeader.appendChild(modalTitle);
+    modalContent.appendChild(modalHeader);
+
+    const modalBody = document.createElement('div');
+    modalBody.classList.add(`${extPrefix}modal_body`);
+    modalContent.appendChild(modalBody);
+
+    const modalFooter = document.createElement('div');
+    modalFooter.classList.add(`${extPrefix}modal_footer`);
+    modalFooter.classList.add(`${extPrefix}d-flex`);
+    const modalActionMain = document.createElement('button');
+    modalActionMain.classList.add(`${extPrefix}modal_actionMain`);
+    modalActionMain.classList.add(`${extPrefix}ml-auto`);
+    modalActionMain.classList.add(`${extPrefix}mr-2`);
+    modalActionMain.classList.add('btn');
+    modalActionMain.classList.add('btn-primary');
+    modalFooter.appendChild(modalActionMain);
+    const modalActionSecond = document.createElement('button');
+    modalActionSecond.classList.add(`${extPrefix}modal_actionSecond`);
+    modalActionSecond.classList.add(`${extPrefix}mr-2`);
+    modalActionSecond.classList.add('btn');
+    modalActionSecond.classList.add('btn-danger');
+    modalFooter.appendChild(modalActionSecond);
+    const modalCloseButton = document.createElement('button');
+    modalCloseButton.classList.add(`${extPrefix}modal_close`);
+    modalCloseButton.classList.add('btn');
+    modalCloseButton.classList.add('btn-gray');
+    modalCloseButton.textContent = browser.i18n.getMessage("closeText");
+    modalFooter.appendChild(modalCloseButton);
+    modalContent.appendChild(modalFooter);
+
+    extModal.appendChild(modalContent);
     document.body.appendChild(extModal);
 
     const extModalClose = extModal.querySelectorAll(`.${extPrefix}modal_close`);
